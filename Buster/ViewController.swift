8c4eb6795be61ebfa705bfaa62a955a7341a29b4
Chaiwat Ungpratansombat <ice@appsynth.net>
2022-09-20 20:28:02 +0700

    initial project

---
+++ Buster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buster
+//
+//  Created by iZE Appsynth on 20/9/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
